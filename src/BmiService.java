0bf262c32d5e66a42a24496d85e512123456a736
Ксения Тютчева <dmitrievna13@mail.ru>
2022-06-23 17:48:32 +0700

    Commit2

--- src/BmiService.java
+++ src/BmiService.java
@@ -3,7 +3,7 @@ public class BmiService {
     public int calculate(double cost) {
         double growth = 1.58;
 
-           double imt = cost / (growth * growth);
+        double imt = cost / (growth * growth);
         return (int) imt;
 
     }
